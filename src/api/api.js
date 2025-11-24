8872a361ed5d0a6aedf27b0dbc2eda7b5e3fdecf
helloservices00-blip <helloservices00@gmail.com>
2025-11-24 19:23:47 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://super-backend-bzin.onrender.com/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: "https://super-backend-bzin.onrender.com/api",
+  headers: { "Content-Type": "application/json" },
 });
 
 export const loginUser = (data) => API.post("/auth/login", data);
 export const registerUser = (data) => API.post("/auth/register", data);
-l
