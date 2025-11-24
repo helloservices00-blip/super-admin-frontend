8af12384694e7bd10b802ddfc39342f813f50997
helloservices00-blip <helloservices00@gmail.com>
2025-11-24 19:26:40 +0530

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,10 +1,18 @@
-import { createContext, useState } from "react";
+// src/context/AuthContext.jsx
+import { createContext, useState, useEffect } from "react";
+import { setAuthToken } from "../api/api.js";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [token, setToken] = useState(localStorage.getItem("token") || null);
+
+  useEffect(() => {
+    if (token) {
+      setAuthToken(token); // attach token to axios
+    }
+  }, [token]);
 
   return (
     <AuthContext.Provider value={{ user, setUser, token, setToken }}>
